b2522a1afe80b470ac161a53d7ac1829f2fe2e3d
Jacek Wilczyński <jacek.wilczynski@bamiz.pl>
2023-09-25 07:26:13 +0200

    Replace hash with title in offer schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,17 +1,14 @@
-import { index, pgTable, serial, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-export type ExistingOffer = typeof offer.$inferSelect;
-export type NewOffer = typeof offer.$inferInsert;
+export type Offer = typeof offer.$inferSelect;
+export type OfferInput = typeof offer.$inferInsert;
 export const offer = pgTable(
     'offer',
     {
         id: serial('id').primaryKey(),
         url: text('url').unique().notNull(),
-        hash: uuid('hash').unique().notNull(),
+        title: text('title').notNull(),
         createdAt: timestamp('created_at').notNull().defaultNow(),
         updatedAt: timestamp('updated_at').notNull().defaultNow(),
     },
-    table => ({
-        hashIndex: index('hash_idx').on(table.hash),
-    }),
 );

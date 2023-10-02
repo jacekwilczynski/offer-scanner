b5cf0c391a4e80bf84b0fd1cec182043c238e2f8
Jacek Wilczyński <jacek.wilczynski@bamiz.pl>
2023-10-02 18:17:12 +0200

    Retry db connection in docker entrypoint

--- docker/node/docker-entrypoint.dev.sh
+++ docker/node/docker-entrypoint.dev.sh
@@ -2,6 +2,17 @@
 set -e
 
 yarn install
-yarn prisma db push
+
+dbConnectionAttemptsLeft=20
+until yarn prisma db push; do
+    dbConnectionAttemptsLeft=$((dbConnectionAttemptsLeft - 1))
+
+    if [ "$dbConnectionAttemptsLeft" -gt 0 ]; then
+        echo "Database schema creation failed. Most likely, the database isn't responding yet. Will retry in 1s."
+    else
+        echo "Database schema creation failed despite 20 attempts. Quitting."
+        exit 1
+    fi
+done
 
 exec "$(dirname "$0")/docker-entrypoint.sh" "$@"

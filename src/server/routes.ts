6bf55175223b24e14cb1a79ffa4e1b49230f0142
Jacek Wilczyński <jacek.wilczynski@bamiz.pl>
2023-10-21 08:40:01 +0200

    Add /new-offers endpoint

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,12 +1,24 @@
 import * as Router from '@koa/router';
+import { runWithServices } from 'src/dependency-injection';
 
 const router = new Router();
 type Context = Parameters<Parameters<typeof router['use']>[1]>[0];
 
 export const routes = router
     .get('/healthcheck', healthcheck)
+    .get('/new-offers', getNewOffers)
     .routes();
 
 async function healthcheck(ctx: Context) {
     ctx.body = 'ok';
+}
+
+async function getNewOffers(ctx: Context) {
+    await runWithServices(
+        ['bufferedNotifier', 'refresh'],
+        async ({ bufferedNotifier, refresh }) => {
+            await refresh.execute();
+            ctx.body = bufferedNotifier.listings;
+        },
+    );
 }
